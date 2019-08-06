aba0c31dbaf59a4956359459acd672c457126d30
FernandaSzAdr <fsandrade25@gmail.com>
2019-08-06 13:24:20 -0300

    Adiciona nova aplicação no settings

--- simplemooc/simplemooc/settings.py
+++ simplemooc/simplemooc/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 
     'simplemooc.core',
+    'simplemooc.courses',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -102,3 +103,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
