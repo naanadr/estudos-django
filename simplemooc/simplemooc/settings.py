55f986e25d7ca0f8280d10672e8ee54513b6384d
FernandaSzAdr <fsandrade25@gmail.com>
2019-08-07 18:28:52 -0300

    Adiciona configurações para envio de email

--- simplemooc/simplemooc/settings.py
+++ simplemooc/simplemooc/settings.py
@@ -105,3 +105,15 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'simplemooc', 'media')
+MEDIA_URL = '/media/'
+
+# E-mails
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'Nome <email@gmail.com>'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'email@gmail.com'
+EMAIL_HOST_PASSWORD = 'senha'
+EMAIL_PORT = 587
+
+CONTACT_EMAIL = 'contato@simplemooc.com'
