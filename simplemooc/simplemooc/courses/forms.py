d050667836484a3a11ed110fd194520467632104
FernandaSzAdr <fsandrade25@gmail.com>
2019-08-08 16:00:51 -0300

    Adiciona formulario de contato

--- simplemooc/simplemooc/courses/forms.py
+++ simplemooc/simplemooc/courses/forms.py
@@ -1,13 +1,14 @@
 from django import forms
-from django.core.mail import send_mail
 from django.conf import settings
+
+from simplemooc.core.mail import send_mail_template
 
 
 class ContactCourse(forms.Form):
 
     name = forms.CharField(label='Nome', max_length=100)
     email = forms.EmailField(label='E-mail')
-    mensagem = forms.CharField(
+    message = forms.CharField(
         label='Mensagem/Dúvida', widget=forms.Textarea
     )
 
@@ -15,10 +16,11 @@ class ContactCourse(forms.Form):
         context = {
             'name': self.cleaned_data['name'],
             'email': self.cleaned_data['email'],
-            'mensagem': self.cleaned_data['mensagem'],
+            'message': self.cleaned_data['message'],
         }
         subject = 'Contato sobre o Curso {}'.format(course)
-        message = 'Nome: {name}; E-mail: {email}, {mensagem}'.format(**context)
 
-        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL,
-                  [settings.CONTACT_EMAIL])
+        template_name = 'courses/contact_email.html'
+        send_mail_template(
+            subject, template_name, context, [settings.CONTACT_EMAIL]
+        )
